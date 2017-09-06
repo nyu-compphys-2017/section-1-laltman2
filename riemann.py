181d342516149de4f5dc499507360c21ee3b3379
Lauren <lauren_altman@brown.edu>
2017-09-06 13:03:10 -0400

    Reimann function

--- riemann.py
+++ riemann.py
@@ -1,5 +1,8 @@
 # This is a python file! The '#' character indicates that the following line is a comment.
 
+from __future__ import division
+import numpy as np
+
 # The following is an example for how to define a function in Python
 # def tells the compiler that hello_world is the name of a function
 # this implementation of hello_world takes a string as an argument,
@@ -10,6 +13,12 @@ def hello_world(name=''):
     print "hello world!"
     print name
     return
-    
-    
+
+def right_reimann_sum(fn, n, a, b):
+    width = (b-a)/n
+    xvals = np.arange(a+width,b+width,width)
+    yvals = fn(xvals)
+    rsum = np.sum(width*yvals)
+    return rsum
+
 #Implement the Riemann Sum approximation for integrals.
